4fc667a86c768765ea4447e58af9055be57dcd07
Jordan Moldow <jmoldow@box.com>
2015-06-16 21:13:29 -0700

    Correct test invocations of `Client.folder()`
    Some tests were calling `Client.folder()` with a `folder_id`
    argument of 0. However, that parameter is supposed to be of type
    `unicode`, not `int`.
    
    Correct all occurrences of `folder(0)` to `folder('0')`.

--- test/functional/test_token_refresh.py
+++ test/functional/test_token_refresh.py
@@ -9,7 +9,7 @@ def test_expired_access_token_is_refreshed(box_oauth, box_client, mock_box):
     # pylint:disable=protected-access
     mock_box.oauth.expire_token(box_oauth._access_token)
     # pylint:enable=protected-access
-    box_client.folder(0).get()
+    box_client.folder('0').get()
     assert len(mock_box.requests) == 6  # GET /authorize, POST /authorize, /token, get_info, refresh /token, get_info
 
 
@@ -19,4 +19,4 @@ def test_expired_refresh_token_raises(box_oauth, box_client, mock_box):
     mock_box.oauth.expire_token(box_oauth._refresh_token)
     # pylint:enable=protected-access
     with pytest.raises(BoxOAuthException):
-        box_client.folder(0).get()
+        box_client.folder('0').get()
