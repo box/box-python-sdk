d3dcd714df56cc99857e0bca176267a26f3b8e5c
Kelsey Morris <kmorris@box.com>
2016-06-16 09:28:51 -0700

    Add trailing commas, fix constructor formatting

--- test/unit/object/test_event.py
+++ test/unit/object/test_event.py
@@ -2,20 +2,20 @@
 
 from __future__ import unicode_literals
 
-import pytest
-
 from boxsdk.object.event import Event
 
 
 def test_init_event(mock_box_session):
-    event = Event(mock_box_session, "f82c3ba03e41f7e8a7608363cc6c0390183c3f83", {
-        "type": "event",
-        "event_id": "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
-        "source": {
-            "type": "folder",
-            "id": "11446498",
-        }
-    })
+    event = Event(
+        mock_box_session,
+        "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
+        {
+            "type": "event",
+            "event_id": "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
+            "source":
+            {
+                "type": "folder",
+                "id": "11446498",
+            },
+        },)
     assert event['type'] == 'event'
-    with pytest.raises(AttributeError):
-        event.get_url()
--- test/unit/object/test_events.py
+++ test/unit/object/test_events.py
@@ -177,20 +177,24 @@ def mock_event():
         "source": {
             "type": "folder",
             "id": "11446498",
-        }
+        },
     }
 
 
 @pytest.fixture()
 def mock_event_object(mock_box_session):
-    return Event(mock_box_session, "f82c3ba03e41f7e8a7608363cc6c0390183c3f83", {
-        "type": "event",
-        "event_id": "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
-        "source": {
-            "type": "folder",
-            "id": "11446498",
-            }
-        })
+    return Event(
+        mock_box_session,
+        "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
+        {
+            "type": "event",
+            "event_id": "f82c3ba03e41f7e8a7608363cc6c0390183c3f83",
+            "source":
+            {
+                "type": "folder",
+                "id": "11446498",
+            },
+        },)
 
 
 @pytest.fixture()
