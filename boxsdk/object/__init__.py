5117f9d55a1ff477217cf13a3a9e454cac553081
Ke Li <kel@splunk.com>
2016-11-17 11:08:20 +0800

    Fix pylint issue

--- boxsdk/object/__init__.py
+++ boxsdk/object/__init__.py
@@ -5,4 +5,5 @@ from __future__ import unicode_literals
 from six.moves import map   # pylint:disable=redefined-builtin
 
 
-__all__ = list(map(str, ['collaboration', 'comment', 'events', 'event', 'file', 'folder', 'group', 'group_membership', 'search', 'task', 'task_assignment', 'user']))
+__all__ = list(map(str, ['collaboration', 'comment', 'events', 'event', 'file', 'folder', 'group', 'group_membership',
+                         'search', 'task', 'task_assignment', 'user']))
