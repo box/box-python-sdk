765524bd3fd1a653d2d5ddc0a8389a0c6515708a
Jeffrey Meadows <jmeadows@box.com>
2017-06-21 16:03:44 -0700

    Fix JWT Token Refresh

--- boxsdk/auth/jwt_auth.py
+++ boxsdk/auth/jwt_auth.py
@@ -291,6 +291,7 @@ class JWTAuth(OAuth2):
         """
         # pylint:disable=unused-argument
         if self._user_id is None:
-            return self.authenticate_instance()
+            new_access_token = self.authenticate_instance()
         else:
-            return self.authenticate_user()
+            new_access_token = self.authenticate_user()
+        return new_access_token, None
