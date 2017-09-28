c83ee732e2d6ee776e02b8159fa45038102bb2f1
Jeffrey Meadows <jmeadows@box.com>
2017-09-28 16:32:11 -0700

    fix tests

--- test/unit/auth/test_jwt_auth.py
+++ test/unit/auth/test_jwt_auth.py
@@ -417,7 +417,7 @@ def app_config_json_content(
         client_id=fake_client_id,
         client_secret=fake_client_secret,
         jwt_key_id=jwt_key_id,
-        private_key=rsa_private_key_bytes.replace("\n", "\\n"),
+        private_key=rsa_private_key_bytes.replace(b"\n", b"\\n"),
         passphrase=json.dumps(rsa_passphrase),
         enterprise_id=json.dumps(fake_enterprise_id),
     )
