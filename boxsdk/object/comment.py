47814725014a1f8594f9b20c5dac4bd685d01351
Matt Willer <mwiller@box.com>
2018-06-15 20:02:15 -0700

    Add extra blank line to satisfy linter

--- boxsdk/object/comment.py
+++ boxsdk/object/comment.py
@@ -7,6 +7,7 @@ import json
 from boxsdk.object.base_object import BaseObject
 from ..util.api_call_decorator import api_call
 
+
 class Comment(BaseObject):
     """An object that represents a comment on an item"""
     _item_type = 'comment'
