09c3a3baa33e7c06a8ea9b588086681273a8536e
Matt Willer <mwiller@box.com>
2018-06-28 15:41:07 -0700

    Fix up BaseAPIJSONObject tests

--- test/unit/object/test_base_api_json_object.py
+++ test/unit/object/test_base_api_json_object.py
@@ -6,11 +6,21 @@ import pytest
 from boxsdk.object.base_api_json_object import BaseAPIJSONObject
 from boxsdk.object.base_object import BaseObject
 from boxsdk.object.folder import Folder
+from boxsdk.util.translator import Translator
 
 
 @pytest.fixture(params=[{'foo': 'bar'}, {'a': {'b': 'c'}}])
 def response(request):
     return request.param
+
+
+@pytest.fixture()
+def default_translator():
+    return Translator()
+
+@pytest.fixture()
+def original_default_translator():
+    return Translator()
 
 
 @pytest.fixture()
@@ -40,34 +50,18 @@ def test_iter(base_api_json_object):
     assert set(all_test_object_keys) == set(all_dictionary_response_keys)
 
 
-def test_meta_registers_new_item_type_in_default_translator(default_translator, original_default_translator):
+def test_meta_registers_new_item_type_in_default_translator(default_translator):
     item_type = u'ƒøø'
 
     class Foo(BaseAPIJSONObject):
         _item_type = item_type
 
     assert default_translator.translate(item_type) is Foo
-    assert (set(default_translator) - set(original_default_translator)) == set([item_type])
 
 
-@pytest.mark.parametrize('subclass', [BaseAPIJSONObject, BaseObject, Folder])
-def test_meta_does_not_register_new_subclass_in_default_translator_if_item_type_is_not_defined_in_namespace(
-        subclass,
-        default_translator,
-        original_default_translator,
-):
-
-    class Foo(subclass):
-        pass
-
-    assert Foo not in default_translator.values()
-    assert default_translator == original_default_translator
-
-
-def test_meta_overrides_registration_if_subclass_redefines_item_type(default_translator, original_default_translator):
+def test_meta_overrides_registration_if_subclass_redefines_item_type(default_translator):
 
     class FolderSubclass(Folder):
         _item_type = 'folder'
 
     assert default_translator.translate('folder') is FolderSubclass
-    assert set(default_translator.keys()) == set(original_default_translator.keys())
