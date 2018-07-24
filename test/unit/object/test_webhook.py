002f6315e5474d1a20546a579ac54d8c501296ca
carycheng <ccheng@box.com>
2018-07-24 11:40:50 -0700

    added update test

--- test/unit/object/test_webhook.py
+++ test/unit/object/test_webhook.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
+import json
 import pytest
 from mock import Mock
 from boxsdk.config import API
 from boxsdk.network.default_network import DefaultNetworkResponse
 from boxsdk.object.webhook import Webhook
-
 
 @pytest.fixture(scope='module')
 def delete_webhook_response():
@@ -41,4 +41,18 @@ def test_get(test_webhook, mock_box_session):
     }
     webhook = test_webhook.get()
     mock_box_session.get.assert_called_once_with(expected_url, headers=None, params=None)
+    assert isinstance(webhook, Webhook)
+
+
+def test_update(test_webhook, mock_box_session):
+    expected_url = '{0}/webhooks/{1}'.format(API.BASE_API_URL, test_webhook.object_id)
+    mock_box_session.put.return_value.json.return_value = {
+        'type': 'webhook',
+        'id': test_webhook.object_id,
+    }
+    data = {
+        'address': 'https://testnotification.com'
+    }
+    webhook = test_webhook.update_info(data)
+    mock_box_session.put.assert_called_once_with(expected_url, data=json.dumps(data), headers=None, params=None)
     assert isinstance(webhook, Webhook)
