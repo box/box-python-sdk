8fcc4b4183c34ee64132f38803351fee1d360cd2
carycheng <ccheng@box.com>
2018-07-24 12:21:55 -0700

    validate webhook message function

--- boxsdk/object/webhook.py
+++ boxsdk/object/webhook.py
@@ -11,3 +11,37 @@ class Webhook(BaseObject):
     """Represents a Box task."""
 
     _item_type = 'webhook'
+
+    @staticmethod
+    def validate_message(body, headers, primary_signature_key, secondary_signature_key):
+
+        primary_signature = compute_signature(body, headers, primary_signature_key)
+
+        if primary_signature and primary_signature_key == headers.box-secondary_signature_key:
+            return True
+
+        secondary_signature = compute_signature(body, headers, secondary_signature_key)
+
+        if secondary_signature and secondary_signature_key == headers.box-signature-secondary:
+            return True
+
+        return False
+
+    @staticmethod
+    def compute_signature(body, headers, signature_key):
+        if signature_key is None:
+            return None
+
+        if headers.box-signature-version != '1':
+            return None
+
+        if headers.box-signature-algorithm != '1':
+            return None
+
+        new_hmac = hmac.new('sha256', signature_key)
+        new_hmac.update(body)
+        new_hmac.update(headers.box-delivery-timestamp)
+
+        signature = new_hmac.digest
+
+        return signature
