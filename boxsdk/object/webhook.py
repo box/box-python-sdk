75029dfd06ac74589284c8c1a6081e638fb19c0d
carycheng <ccheng@box.com>
2018-07-31 13:03:58 -0700

    addded tests for webhooks message

--- boxsdk/object/webhook.py
+++ boxsdk/object/webhook.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
+import base64
 import json
 from .base_object import BaseObject
 from boxsdk.config import API
@@ -11,18 +12,18 @@ from ..pagination.marker_based_object_collection import MarkerBasedObjectCollect
 def _compute_signature(body, headers, signature_key):
     if signature_key is None:
         return None
-# convert to braces
-    if headers.box-signature-version != '1':
+
+    if headers['box-signature-version'] != '1':
         return None
 
-    if headers.box-signature-algorithm != '1':
+    if headers['box-signature-algorithm'] != '1':
         return None
 
     new_hmac = hmac.new('sha256', signature_key)
     new_hmac.update(body)
-    new_hmac.update(headers.box-delivery-timestamp)
+    new_hmac.update(headers['box-delivery-timestamp'])
 
-    signature = new_hmac.digest
+    signture = base64.b64encode(new_hmac.digest()).decode()
 
     return signature
 
@@ -30,18 +31,20 @@ class Webhook(BaseObject):
     """Represents a Box task."""
 
     _item_type = 'webhook'
-# secondary signature key is optional 
+
     @staticmethod
-    def validate_message(body, headers, primary_signature_key, secondary_signature_key):
+    def validate_message(body, headers, primary_signature_key, secondary_signature_key=None):
 
         primary_signature = _compute_signature(body, headers, primary_signature_key)
 
-        if primary_signature and primary_signature_key == headers.box_secondary_signature_key:
+        if primary_signature and primary_signature_key == headers['box_secondary_signature_key']:
             return True
 
         secondary_signature = _compute_signature(body, headers, secondary_signature_key)
 
-        if secondary_signature and secondary_signature_key == headers.box-signature-secondary:
-            return True
+        if secondary_signature_key:
+            if secondary_signature and secondary_signature_key == headers['box-signature-secondary']:
+                return True
+            return False
 
         return False
--- test/unit/object/test_webhook.py
+++ test/unit/object/test_webhook.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals
 import json
 import pytest
+
+# from dateutil.parser import parse
 from mock import Mock
 from boxsdk.config import API
 from boxsdk.network.default_network import DefaultNetworkResponse
@@ -56,3 +58,35 @@ def test_update(test_webhook, mock_box_session):
     webhook = test_webhook.update_info(data)
     mock_box_session.put.assert_called_once_with(expected_url, data=json.dumps(data), headers=None, params=None)
     assert isinstance(webhook, Webhook)
+
+
+def test_validate_message():
+    body = {
+        'type': 'webhook_event',
+        'webhook': {
+            'id': '1234567890',
+        },
+        'trigger': 'FILE.UPLOADED',
+        'source': {
+            'id': '1234567890',
+            'type': 'file',
+            'name': 'Test.txt',
+        }
+    }
+    headers = {
+        'box-delivery-id': 'f96bb54b-ee16-4fc5-aa65-8c2d9e5b546f',
+        'box-delivery-timestamp': '2020-01-01T00:00:00-07:00',
+        'box-signature-algorithm': 'HmacSHA256',
+        'box-signature-primary': '6TfeAW3A1PASkgboxxA5yqHNKOwFyMWuEXny/FPD5hI=',
+        'box-signature-secondary': 'v+1CD1Jdo3muIcbpv5lxxgPglOqMfsNHPV899xWYydo=',
+        'box-signature-version': '1'
+    }
+    primary_signature_key = 'SamplePrimaryKey'
+    secondary_signature_key = 'SampleSecondaryKey'
+    # incorrect_signaute_key = 'IncorrectKey'
+    # date_in_past = parse('2010-01-01T00:00:00-07:00')
+    # date_in_future = parse('2020-01-01T00:15:00-07:00')
+    # headers_with_wrong_signature_version = headers.update({'box-signature-version': '2'})
+    # headers_with_wrong_signature_algorithm = headers.update({'box-signature-algorithm': 'XXX'})
+
+    Webhook.validate_message(body, headers, primary_signature_key, secondary_signature_key)
