114569700352c1663bef0117ecde12ed3bd765dc
carycheng <ccheng@box.com>
2018-07-31 19:10:28 -0700

    added tests for validate webhook message

--- boxsdk/object/webhook.py
+++ boxsdk/object/webhook.py
@@ -2,30 +2,50 @@
 
 from __future__ import unicode_literals
 import base64
+import hmac
 import json
 from .base_object import BaseObject
 from boxsdk.config import API
 from boxsdk.util.translator import Translator
-from ..pagination.marker_based_object_collection import MarkerBasedObjectCollection
 
 
 def _compute_signature(body, headers, signature_key):
+    """
+    Computes the Hmac for the webhook notification given one signature key.
+
+    :param body:
+        The encoded webhook body.
+    :type:
+        `bytes`
+    :param headers:
+        The headers for the `Webhook` notification.
+    :type headers:
+        `dict`
+    :param signature_key:
+        The `Webhook` signature key for this application.
+    :type signature_key:
+        `unicode`
+    """
     if signature_key is None:
         return None
 
-    if headers['box-signature-version'] != '1':
+    if headers.get('box-signature-version') != '1':
         return None
 
-    if headers['box-signature-algorithm'] != '1':
+    if headers.get('box-signature-algorithm') != 'HmacSHA256':
         return None
 
-    new_hmac = hmac.new('sha256', signature_key)
+    encoded_signature_key = signature_key.encode()
+    encoded_delivery_time_stamp = headers.get('box-delivery-timestamp').encode()
+
+    new_hmac = hmac.new(encoded_signature_key, digestmod='sha256')
     new_hmac.update(body)
-    new_hmac.update(headers['box-delivery-timestamp'])
+    new_hmac.update(encoded_delivery_time_stamp)
 
-    signture = base64.b64encode(new_hmac.digest()).decode()
+    signature = base64.b64encode(new_hmac.digest()).decode()
 
     return signature
+
 
 class Webhook(BaseObject):
     """Represents a Box task."""
@@ -34,16 +54,37 @@ class Webhook(BaseObject):
 
     @staticmethod
     def validate_message(body, headers, primary_signature_key, secondary_signature_key=None):
+        """
+        Validates a `Webhook` message.
+
+        :param body:
+            The encoded webhook body.
+        :type:
+            `bytes`
+        :param headers:
+            The headers for the `Webhook` notification.
+        :type headers:
+            `dict`
+        :param primary_signature_key:
+            The `Webhook` primary signature key for this application.
+        :type primary_signature_key:
+            `unicode`
+        :param secondary_signature_key:
+            The `Webhook` secondary signature key for this application.
+        :type secondary_signature_key:
+            `unicode`
+        """
+        if type(body) is dict:
+            body = json.dumps(body, separators=(',', ':')).encode()
 
         primary_signature = _compute_signature(body, headers, primary_signature_key)
 
-        if primary_signature and primary_signature_key == headers['box_secondary_signature_key']:
+        if primary_signature and primary_signature == headers.get('box-signature-primary'):
             return True
 
-        secondary_signature = _compute_signature(body, headers, secondary_signature_key)
-
         if secondary_signature_key:
-            if secondary_signature and secondary_signature_key == headers['box-signature-secondary']:
+            secondary_signature = _compute_signature(body, headers, secondary_signature_key)
+            if secondary_signature and secondary_signature == headers.get('box-signature-secondary'):
                 return True
             return False
 
--- test/unit/object/test_webhook.py
+++ test/unit/object/test_webhook.py
@@ -29,10 +29,8 @@ def test_delete_webhook_return_the_correct_response(
     response = test_webhook.delete()
     # pylint:disable=protected-access
     expected_url = test_webhook.get_url()
-    # pylint:enable = protected-access
     mock_box_session.delete.assert_called_once_with(expected_url, params={}, expect_json_response=False, headers=None)
     assert response is True
-
 
 
 def test_get(test_webhook, mock_box_session):
@@ -60,7 +58,19 @@ def test_update(test_webhook, mock_box_session):
     assert isinstance(webhook, Webhook)
 
 
-def test_validate_message():
+@pytest.mark.parametrize(
+    'signature_version,signature_algorithm,primary_key,secondary_key,expected_result',
+    [
+        ('1', 'HmacSHA256', 'SamplePrimaryKey', 'SampleSecondaryKey', True),
+        ('1', 'HmacSHA256', 'SamplePrimaryKey', None, True),
+        ('1', 'HmacSHA256', 'WrongPrimaryKey', 'SampleSecondaryKey', True),
+        ('1', 'HmacSHA256', 'WrongPrimaryKey', 'WrongSecondaryKey', False),
+        ('1', 'HmacSHA256', None, None, False),
+        ('2', 'HmacSHA256', 'SamplePrimaryKey', 'SampleSecondaryKey', False),
+        ('1', 'WrongAlgorithm', 'SamplePrimaryKey', 'SampleSecondaryKey', False),
+    ]
+)
+def test_validate_message(signature_version, signature_algorithm, primary_key, secondary_key, expected_result):
     body = {
         'type': 'webhook_event',
         'webhook': {
@@ -73,20 +83,15 @@ def test_validate_message():
             'name': 'Test.txt',
         }
     }
+    encoded_body = json.dumps(body, separators=(',', ':')).encode()
     headers = {
         'box-delivery-id': 'f96bb54b-ee16-4fc5-aa65-8c2d9e5b546f',
         'box-delivery-timestamp': '2020-01-01T00:00:00-07:00',
-        'box-signature-algorithm': 'HmacSHA256',
+        'box-signature-algorithm': signature_algorithm,
         'box-signature-primary': '6TfeAW3A1PASkgboxxA5yqHNKOwFyMWuEXny/FPD5hI=',
         'box-signature-secondary': 'v+1CD1Jdo3muIcbpv5lxxgPglOqMfsNHPV899xWYydo=',
-        'box-signature-version': '1',
+        'box-signature-version': signature_version,
     }
-    primary_signature_key = 'SamplePrimaryKey'
-    secondary_signature_key = 'SampleSecondaryKey'
-    # incorrect_signaute_key = 'IncorrectKey'
-    # date_in_past = parse('2010-01-01T00:00:00-07:00')
-    # date_in_future = parse('2020-01-01T00:15:00-07:00')
-    # headers_with_wrong_signature_version = headers.update({'box-signature-version': '2'})
-    # headers_with_wrong_signature_algorithm = headers.update({'box-signature-algorithm': 'XXX'})
 
-    Webhook.validate_message(body, headers, primary_signature_key, secondary_signature_key)
+    is_validated = Webhook.validate_message(encoded_body, headers, primary_key, secondary_key)
+    assert is_validated is expected_result
