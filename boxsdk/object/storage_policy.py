eff576bc2a7cfc6d78741aeefba596f245ec3cc6
carycheng <ccheng@box.com>
2018-09-17 11:17:01 -0700

    removed unused lines

--- boxsdk/object/storage_policy.py
+++ boxsdk/object/storage_policy.py
@@ -42,7 +42,6 @@ class StoragePolicy(BaseObject):
             }
         }
         response = self._session.post(url, data=json.dumps(body)).json()
-        if response.status is 
         return self.translator.translate(response['type'])(
             self._session,
             response['id'],
