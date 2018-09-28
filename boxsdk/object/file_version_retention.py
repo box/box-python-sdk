0953497167743387e882b8a7820b1320aaf4ee14
carycheng <ccheng@box.com>
2018-09-28 16:51:01 -0700

    Retention Policies v2 (#328)

---
+++ boxsdk/object/file_version_retention.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+from .base_object import BaseObject
+
+
+class FileVersionRetention(BaseObject):
+    """Represents a Box file version retention."""
+    _item_type = 'file_version_retention'
---
+++ boxsdk/object/retention_policy_assignment.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from __future__ import unicode_literals, absolute_import
+from .base_object import BaseObject
+
+
+class RetentionPolicyAssignment(BaseObject):
+    """Represents a Box retention policy assignment."""
+    _item_type = 'retention_policy_assignment'
