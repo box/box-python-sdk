afc6ee98599566f360f59c282d6294de98695559
carycheng <ccheng@box.com>
2018-10-24 21:30:00 -0700

    added test for storage policy and storage policy constructor returns correct translated objects

--- test/unit/client/test_client.py
+++ test/unit/client/test_client.py
@@ -25,6 +25,7 @@ from boxsdk.object.folder import Folder
 from boxsdk.object.file import File
 from boxsdk.object.group import Group
 from boxsdk.object.storage_policy import StoragePolicy
+from boxsdk.object.storage_policy_assignment import StoragePolicyAssignment
 from boxsdk.object.user import User
 from boxsdk.object.trash import Trash
 from boxsdk.object.group_membership import GroupMembership
@@ -341,7 +342,9 @@ def device_pins_response(device_pin_id_1, device_pin_id_2):
     (Group, 'group'),
     (GroupMembership, 'group_membership'),
     (Enterprise, 'enterprise'),
-    (Webhook, 'webhook')
+    (Webhook, 'webhook'),
+    (StoragePolicy, 'storage_policy'),
+    (StoragePolicyAssignment, 'storage_policy_assignment'),
 ])
 def test_factory_returns_the_correct_object(mock_client, test_class, factory_method_name):
     """ Tests the various id-only factory methods in the Client class """
