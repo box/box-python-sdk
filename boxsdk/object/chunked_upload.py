ad82d9e83515b82ad95ae36a723e92a8b8ee4851
carycheng <ccheng@box.com>
2018-11-16 14:57:32 -0800

    created functions for reading chunk and uploading

--- boxsdk/object/chunked_upload.py
+++ boxsdk/object/chunked_upload.py
@@ -9,46 +9,40 @@ from ..pagination.limit_offset_based_dict_collection import LimitOffsetBasedDict
 
 class ChunkedUpload(object):
     _item_type = 'chunked_upload'
+    _uploaded_part = None
+    _part_array = []
+    _sha1 = hashlib.sha1()
 
     def __init__(self, upload_session, content_stream, file_size):
         self._upload_session = upload_session
         self._content_stream = content_stream
         self._file_size = file_size
 
-    # def get_url(self, *args):
-    #     """
-    #     Base class override. Endpoint is a little different - it's /files/upload_sessions.
-
-    #     :rtype:
-    #         `unicode`
-    #     """
-    #     return self._session.get_url(
-    #         '{0}s/{1}s'.format(self._parent_item_type, self._item_type),
-    #         self._object_id,
-    #         *args
-    #     ).replace(self.session.api_config.BASE_API_URL, self.session.api_config.UPLOAD_URL)
-
     def start(self):
-        part_array = []
-        sha1 = hashlib.sha1()
-        for part_num in range(self._upload_session.total_parts):
-            copied_length = 0
-            chunk = b''
-            while copied_length < self._upload_session.part_size:
-                bytes_read = self._content_stream.read(self._upload_session.part_size - copied_length)
-                if bytes_read is None:
-                    # stream returns none when no bytes are ready currently but there are
-                    # potentially more bytes in the stream to be read.
-                    continue
-                if len(bytes_read) == 0:
-                    # stream is exhausted.
-                    break
-                chunk += bytes_read
-                copied_length += len(bytes_read)
+        self._upload()
+        content_sha1 = self._sha1.digest()
+        self._upload_session.commit(content_sha1=content_sha1, parts=self._part_array)
 
-            uploaded_part = self._upload_session.upload_part_bytes(chunk, part_num*self._upload_session.part_size,
-                                                                   self._file_size)
-            part_array.append(uploaded_part)
-            sha1.update(chunk)
-        content_sha1 = sha1.digest()
-        self._upload_session.commit(content_sha1=content_sha1, parts=part_array)
+    def _upload(self):
+        while len(self._part_array) < self._upload_session.total_parts:
+            chunk = self._read_chunk()
+            self._uploaded_part = self._upload_session.upload_part_bytes(chunk, len(self._part_array)*self._upload_session.part_size,
+                                                                         self._file_size)
+            self._part_array.append(self._uploaded_part)
+            self._sha1.update(chunk)
+
+    def _read_chunk(self):
+        copied_length = 0
+        chunk = b''
+        while copied_length < self._upload_session.part_size:
+            bytes_read = self._content_stream.read(self._upload_session.part_size - copied_length)
+            if bytes_read is None:
+                # stream returns none when no bytes are ready currently but there are
+                # potentially more bytes in the stream to be read.
+                continue
+            if len(bytes_read) == 0:
+                # stream is exhausted.
+                break
+            chunk += bytes_read
+            copied_length += len(bytes_read)
+        return chunk
