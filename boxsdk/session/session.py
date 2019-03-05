c26317bade228b5c61e47edd27f8551b10162367
carycheng <ccheng@box.com>
2019-03-05 11:52:23 -0800

    add docstring

--- boxsdk/session/session.py
+++ boxsdk/session/session.py
@@ -424,11 +424,15 @@ class Session(object):
 
     def _get_request_headers(self):
         return self._default_headers.copy()
-    
-    # private function here to construct proxy i.e. authenticated, non-authenticated
+
     def _prepare_proxy(self):
         """
-        Sets basic authenticated and unauthenticated proxies for requests.
+        Prepares basic authenticated and unauthenticated proxies for requests.
+
+        :return:
+            A prepared proxy dict to send along with the request.
+        :rtype:
+            `dict`
         """
         proxy = {}
         if API.PROXY_AUTH is not None:
@@ -459,7 +463,6 @@ class Session(object):
         request_kwargs.update(kwargs)
         proxy_dict = self._prepare_proxy()
         request_kwargs.update({'proxies': proxy_dict})
-        # attached ('proxies', proxy) here
         if files and file_stream_positions:
             for name, position in file_stream_positions.items():
                 files[name][1].seek(position)
