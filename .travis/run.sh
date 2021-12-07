45164c12f945c71efd041c4777e8ff283c86a8e6
Lukasz Socha <lsocha@box.com>
2021-12-07 13:07:12 +0100

    chore: fix travis for python 2.7
    Closes: SDK-1870

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,7 +10,7 @@ set -o pipefail
 if [[ "$(uname -s)" == "Darwin" ]]; then
     eval "$(pyenv init -)"
 else
-    if [[ "${TOX_ENV}" == "pypy" ]]; then
+    if [[ "${TOX_ENV}" == "pypy" ]] || [[ "${TOX_ENV}" == "py27" ]] || [[ "${TOX_ENV}" == "pycodestyle" ]] || [[ "${TOX_ENV}" == "pylint" ]]; then
         export PYENV_ROOT="$PWD/.pyenv"
         export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
