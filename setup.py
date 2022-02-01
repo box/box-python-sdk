9f7b8dc90fee709aaaa5302479ff7cea41819b7f
Lukasz Socha <lsocha@box.com>
2022-02-01 15:39:17 +0100

    enable coveralls

--- setup.py
+++ setup.py
@@ -72,7 +72,8 @@ def main():
         'pytest>=2.8.3, <4.0.0',
         'pytest-cov',
         'pytest-xdist<1.28.0',
-        'coverage',  # 2020-01-14: backwards-compatibility issue with coverage 5.0 impacting python-coveralls
+        'coveralls',
+        'coverage',
         'pytz',
     ]
     extra_requires['test'] = test_requires
