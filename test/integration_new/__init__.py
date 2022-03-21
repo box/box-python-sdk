abcb1edf75d9e8172ef3ad89bc1e0174926bd345
Lukasz Socha <lsocha@box.com>
2022-03-21 13:22:48 +0100

    fix lint

--- test/integration_new/__init__.py
+++ test/integration_new/__init__.py
@@ -12,7 +12,7 @@ def read_jwt_path_from_config(config_path: str):
     return config_parser["JWT"].get('SettingsFilePath')
 
 
-current_dir_path = str(Path(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))))
-jwt_config_path = read_jwt_path_from_config(config_path=f'{current_dir_path}/integration_tests.cfg')
+CURRENT_DIR_PATH = str(Path(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))))
+jwt_config_path = read_jwt_path_from_config(config_path=f'{CURRENT_DIR_PATH}/integration_tests.cfg')
 jwt_config = JWTAuth.from_settings_file(jwt_config_path)
 CLIENT = Client(jwt_config)
