2f629590911f0470e078d9c22de5e3850baf5bc7
lsocha <lsocha@box.com>
2022-04-25 17:35:53 +0200

    fix style

--- test/integration_new/__init__.py
+++ test/integration_new/__init__.py
@@ -13,7 +13,9 @@ def read_jwt_path_from_env_var() -> dict:
 
     jwt_config_base64 = os.getenv(JWT_CONFIG_ENV_VAR_NAME)
     if not jwt_config_base64:
-        raise RuntimeError("JWT config cannot be loaded. Missing required environment variable: JWT_CONFIG_BASE_64.")
+        raise RuntimeError(
+            f'JWT config cannot be loaded. Missing required environment variable: {JWT_CONFIG_ENV_VAR_NAME}.'
+        )
     jwt_config_str = base64.b64decode(jwt_config_base64)
 
     return json.loads(jwt_config_str)
