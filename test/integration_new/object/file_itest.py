57ad1a649a58b3198c60b6cea8020b34a18d0dab
Minh Nguyen Cong <mcong@box.com>
2022-07-27 15:05:12 +0200

    chore: Add integration test for webhook

--- test/integration_new/object/file_itest.py
+++ test/integration_new/object/file_itest.py
@@ -11,6 +11,8 @@ from test.integration_new.context_managers.box_retention_policy import BoxRetent
 from test.integration_new import util
 from test.integration_new.context_managers.box_test_file import BoxTestFile
 from test.integration_new.context_managers.box_test_folder import BoxTestFolder
+from test.integration_new.context_managers.box_sign_request import BoxTestSignRequest
+from test.integration_new import CLIENT
 
 
 FILE_TESTS_DIRECTORY_NAME = 'file-integration-tests'
@@ -244,6 +246,50 @@ def test_copy(test_file, parent_folder):
     assert test_file.content() == copied_file.content()
 
     util.permanently_delete(copied_file)
+
+
+def test_webhook_sign_request(parent_folder, small_file_path):
+    with BoxTestFile(parent_folder=parent_folder, file_path=small_file_path) as test_file:
+        file = {
+            'id': test_file.object_id,
+            'type': test_file.object_type,
+        }
+        files = [file]
+        signer = {
+            'email': 'signer@mail.com',
+        }
+        signers = [signer]
+
+        with BoxTestSignRequest(files=files, signers=signers, parent_folder_id=parent_folder.id) as sign_request:
+            webhook_url = 'https://example.com/webhook'
+            new_webhook_url = 'https://example.com/new-webhook'
+
+            assert len(sign_request.sign_files['files']) == 1
+
+            sign_file = sign_request.sign_files['files'][0]
+            webhook_file = CLIENT.file(file_id=sign_file['id'])
+            webhook = CLIENT.create_webhook(
+                webhook_file,
+                ['SIGN_REQUEST.COMPLETED', 'SIGN_REQUEST.DECLINED', 'SIGN_REQUEST.EXPIRED'],
+                webhook_url
+            )
+
+            assert webhook.id
+            assert len(webhook.triggers) == 3
+            assert webhook.address == webhook_url
+
+            update_data = {
+                'address': new_webhook_url,
+                'triggers': ['SIGN_REQUEST.COMPLETED']
+            }
+            webhook = webhook.update_info(data=update_data)
+
+            assert webhook.address == new_webhook_url
+            assert len(webhook.triggers) == 1
+
+            webhook.delete()
+            with pytest.raises(BoxAPIException):
+                webhook.get()
 
 
 def test_set_disposition_at(parent_folder, small_file_path):
