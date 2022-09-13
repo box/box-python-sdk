a279a4e5c2cf37bb90037951a5997775b22d0578
lsocha <lsocha@box.com>
2022-09-13 13:46:23 +0200

    add retries for avatar itest

--- test/integration_new/object/user_itest.py
+++ test/integration_new/object/user_itest.py
@@ -8,7 +8,15 @@ def test_upload_and_delete_avatar(user, image_path):
         avatar_urls = user.upload_avatar(image_path)
         assert 'large' in avatar_urls and 'preview' in avatar_urls and 'small' in avatar_urls
 
-        avatar = user.get_avatar()
+        for _ in range(3):
+            try:
+                avatar = user.get_avatar()
+            except BoxAPIException:
+                continue
+            break
+        else:
+            raise Exception("Cannot get avatar")
+
         assert avatar
     finally:
         user.delete_avatar()
