7092957b09d76427d44a8fa598033e15d466975e
box-sdk-build <sdk-build@box.com>
2025-10-02 07:37:41 -0700

    fix: Mark fields in `AiExtractResponse` and `Event` with additionalProperties (box/box-openapi#556)

--- box_sdk_gen/schemas/ai_extract_response.py
+++
@@ -1,8 +0,0 @@
-from box_sdk_gen.internal.base_object import BaseObject
-
-from box_sdk_gen.box.errors import BoxSDKError
-
-
-class AiExtractResponse(BaseObject):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
