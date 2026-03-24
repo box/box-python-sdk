954ac510dd6c75092c5759de1daf352d9006a4cc
box-sdk-build <sdk-build@box.com>
2026-03-24 10:10:13 -0700

    docs: Fix legal hold policy docs (box/box-openapi#1554) (box/box-openapi#590)

--- box_sdk_gen/schemas/legal_hold_policy_assigned_item.py
+++ box_sdk_gen/schemas/legal_hold_policy_assigned_item.py
@@ -1,11 +1,36 @@
-from typing import Union
+from enum import Enum
 
-from box_sdk_gen.schemas.file import File
-
-from box_sdk_gen.schemas.folder import Folder
-
-from box_sdk_gen.schemas.web_link import WebLink
+from box_sdk_gen.internal.base_object import BaseObject
 
 from box_sdk_gen.box.errors import BoxSDKError
 
-LegalHoldPolicyAssignedItem = Union[File, Folder, WebLink]
+
+class LegalHoldPolicyAssignedItemTypeField(str, Enum):
+    FILE = 'file'
+    FILE_VERSION = 'file_version'
+    FOLDER = 'folder'
+    USER = 'user'
+    OWNERSHIP = 'ownership'
+    INTERACTIONS = 'interactions'
+
+
+class LegalHoldPolicyAssignedItem(BaseObject):
+    _discriminator = 'type', {
+        'file',
+        'file_version',
+        'folder',
+        'user',
+        'ownership',
+        'interactions',
+    }
+
+    def __init__(self, type: LegalHoldPolicyAssignedItemTypeField, id: str, **kwargs):
+        """
+        :param type: The type of item the policy is assigned to.
+        :type type: LegalHoldPolicyAssignedItemTypeField
+        :param id: The ID of the item the policy is assigned to.
+        :type id: str
+        """
+        super().__init__(**kwargs)
+        self.type = type
+        self.id = id
