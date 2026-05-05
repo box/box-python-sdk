d31bca44f67decd8e2a65eab8d95f45971bba591
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-05-05 18:29:15 +0200

    fix!: Mark `id` field of `AIAgentReference` required (box/box-openapi#1629) (box/box-openapi#595) (#1422)

--- box_sdk_gen/schemas/ai_agent_reference.py
+++ box_sdk_gen/schemas/ai_agent_reference.py
@@ -1,7 +1,5 @@
 from enum import Enum
 
-from typing import Optional
-
 from box_sdk_gen.internal.base_object import BaseObject
 
 from box_sdk_gen.box.errors import BoxSDKError
@@ -16,19 +14,19 @@ class AiAgentReference(BaseObject):
 
     def __init__(
         self,
+        id: str,
         *,
         type: AiAgentReferenceTypeField = AiAgentReferenceTypeField.AI_AGENT_ID,
-        id: Optional[str] = None,
         **kwargs
     ):
         """
-                :param type: The type of AI agent used to handle queries., defaults to AiAgentReferenceTypeField.AI_AGENT_ID
-                :type type: AiAgentReferenceTypeField, optional
                 :param id: The ID of an Agent. This can be a numeric ID for custom agents (for example, `14031`)
         or a unique identifier for pre-built agents (for example, `enhanced_extract_agent`
-        for the [Enhanced Extract Agent](https://developer.box.com/guides/box-ai/ai-tutorials/extract-metadata-structured#enhanced-extract-agent))., defaults to None
-                :type id: Optional[str], optional
+        for the [Enhanced Extract Agent](https://developer.box.com/guides/box-ai/ai-tutorials/extract-metadata-structured#enhanced-extract-agent)).
+                :type id: str
+                :param type: The type of AI agent used to handle queries., defaults to AiAgentReferenceTypeField.AI_AGENT_ID
+                :type type: AiAgentReferenceTypeField, optional
         """
         super().__init__(**kwargs)
-        self.type = type
         self.id = id
+        self.type = type
